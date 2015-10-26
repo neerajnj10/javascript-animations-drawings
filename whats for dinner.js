66f7a0a8055d5236b0575daebcce7520d9952257
Neeraj Kumar <nekumar@mix.wvu.edu>
2015-10-26 14:59:14 -0400

    Create whats for dinner.js

---
+++ whats for dinner.js
@@ -0,0 +1,16 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+fill(255, 255, 0);
+triangle(266, 238, 197, 269, 171, 93);//Cheese Pizza
+triangle(63, 281, 171, 273, 105, 95);//Pepperoni Pizza
+fill(255, 0, 0);
+ellipse(104, 232, 24, 24);
+ellipse(101, 187, 24, 24);
+ellipse(134, 261, 24, 24);
+ellipse(128, 205, 24, 24);
+ellipse(97, 262, 24, 24);
+ellipse(114, 161, 24, 24);
+fill(158, 112, 112);
+ellipse(43, 49, 80, 80);//chocolate milk
